2f5f1f5e6d28b7441b8e98300d00f809263f5de4
goknurbilisim1 <145705901+goknurbilisim1@users.noreply.github.com>
2023-10-23 13:15:42 +0300

    Add files via upload

---
+++ ornek-1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ornek_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyazdir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstSayilar = new System.Windows.Forms.ListBox();
+            this.txtBaslangic = new System.Windows.Forms.TextBox();
+            this.txtBitis = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnyazdir
+            // 
+            this.btnyazdir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnyazdir.Location = new System.Drawing.Point(124, 79);
+            this.btnyazdir.Name = "btnyazdir";
+            this.btnyazdir.Size = new System.Drawing.Size(106, 36);
+            this.btnyazdir.TabIndex = 0;
+            this.btnyazdir.Text = "button1";
+            this.btnyazdir.UseVisualStyleBackColor = true;
+            this.btnyazdir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Başlangıç:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(52, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bitiş:";
+            // 
+            // lstSayilar
+            // 
+            this.lstSayilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstSayilar.FormattingEnabled = true;
+            this.lstSayilar.ItemHeight = 24;
+            this.lstSayilar.Location = new System.Drawing.Point(269, 10);
+            this.lstSayilar.Name = "lstSayilar";
+            this.lstSayilar.Size = new System.Drawing.Size(148, 196);
+            this.lstSayilar.TabIndex = 3;
+            // 
+            // txtBaslangic
+            // 
+            this.txtBaslangic.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBaslangic.Location = new System.Drawing.Point(124, 10);
+            this.txtBaslangic.Name = "txtBaslangic";
+            this.txtBaslangic.Size = new System.Drawing.Size(106, 29);
+            this.txtBaslangic.TabIndex = 4;
+            // 
+            // txtBitis
+            // 
+            this.txtBitis.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBitis.Location = new System.Drawing.Point(124, 44);
+            this.txtBitis.Name = "txtBitis";
+            this.txtBitis.Size = new System.Drawing.Size(106, 29);
+            this.txtBitis.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 363);
+            this.Controls.Add(this.txtBitis);
+            this.Controls.Add(this.txtBaslangic);
+            this.Controls.Add(this.lstSayilar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnyazdir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnyazdir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstSayilar;
+        private System.Windows.Forms.TextBox txtBaslangic;
+        private System.Windows.Forms.TextBox txtBitis;
+    }
+}
+
---
+++ ornek-1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ornek_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int baslangic=Convert.ToInt32(txtBaslangic.Text);
+            int bitis=Convert.ToInt32(txtBitis.Text);
+ 
+           for(int i = baslangic; i <=bitis; i++)
+            {
+                lstSayilar.Items.Add(i);
+            }
+
+           
+           
+
+
+        }
+    }
+}
